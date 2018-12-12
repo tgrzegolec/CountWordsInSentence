4f0667f9ed629a1c7e55f0bbf3bb762b41584a44
Tomasz Grzegolec <tomasz.grzegolec@o2.pl>
2018-12-12 19:28:08 +0100

    first commit CountWords

---
+++ CountWordsInSentence/CountWordsInSentence/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CountWordsInSentence
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBox.Text = "This is a statement, and so is this";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] sentence = TextBox.Text.ToLower().Split(new char[] { ' ', '.', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            var result = sentence.GroupBy(w => w).ToDictionary(w => w.Key, c => c.Count()).OrderByDescending(desc => desc.Value);
+
+            ListBox.ItemsSource = result.ToList();
+        }
+    }
+}
